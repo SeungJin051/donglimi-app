cf06738128ec07bdace187e4e6a41b0dfc07efe5
임승진 <seungjin051@gmail.com>
2025-09-24 23:49:01 +0900

    feat:Custom HomeHeader Dev

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,12 +1,21 @@
-import { Stack } from 'expo-router';
+import 'react-native-gesture-handler';
+
+import { Drawer } from 'expo-router/drawer';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import '../../global.css';
+import HomeDrawer from '@/components/layout/HomeDrawer/HomeDrawer';
 
 // _를 붙이면 일반 페이지가 아니라 레이아웃(layout), 슬롯(slot), API 라우트 등 특별한 기능을 하는 파일로 취급됩니다.
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
-    </Stack>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Drawer drawerContent={() => <HomeDrawer />}>
+        <Drawer.Screen
+          name='(tabs)'
+          options={{ drawerLabel: '홈', title: '홈', headerShown: false }}
+        />
+      </Drawer>
+    </GestureHandlerRootView>
   );
 }
