68f7a3198cb7aef56f209634366964a2e6251204
임승진 <seungjin051@gmail.com>
2025-09-25 22:51:13 +0900

    feat: add react-native-reanimated and setting

--- metro.config.cjs
+++ metro.config.cjs
@@ -1,13 +1,17 @@
 // metro.config.js
-
 const { getDefaultConfig } = require('expo/metro-config')
 const { withNativeWind } = require('nativewind/metro')
+const {
+  wrapWithReanimatedMetroConfig,
+} = require('react-native-reanimated/metro-config')
 
+// 기본 Expo Metro 설정을 가져옵니다.
 const config = getDefaultConfig(__dirname)
 
-module.exports = withNativeWind(config, {
-  // 여기에 NativeWind 관련 추가 설정을 할 수 있습니다.
+// NativeWind 설정을 적용합니다.
+const configWithNativeWind = withNativeWind(config, {
   input: './global.css',
-  // React Native 웹을 사용한다면 아래 설정을 추가하세요.
-  // projectRoot: __dirname,
 })
+
+// NativeWind가 적용된 설정을 Reanimated 설정으로 다시 감싸서 내보냅니다.
+module.exports = wrapWithReanimatedMetroConfig(configWithNativeWind)
