b5687a19f39606c84c3da7555aa2688f58219bcf
임승진 <seungjin051@gmail.com>
2025-09-26 16:04:02 +0900

    feat: add departments constants file

---
+++ src/constants/departments.ts
@@ -0,0 +1,123 @@
+export const SUBSCRIPTION_LIST = [
+  // 정보광장
+  { category: '정보광장', name: '일반공지', type: '공지사항' },
+  { category: '정보광장', name: '장학공지', type: '공지사항' },
+  { category: '정보광장', name: '교육·모집', type: '공지사항' },
+  { category: '정보광장', name: '기숙사', type: '공지사항' },
+  { category: '정보광장', name: '채용', type: '채용공지' },
+  { category: '정보광장', name: '입찰', type: '공지사항' },
+  { category: '정보광장', name: '외부기관', type: '공지사항' },
+
+  // 동의소식
+  { category: '동의소식', name: '동의 TODAY', type: '동의TODAY' },
+
+  // 인문사회과학대학
+  { category: '인문사회과학대학', name: '국어국문학과', type: '학과공지' },
+  { category: '인문사회과학대학', name: '유아교육과', type: '학과공지' },
+  { category: '인문사회과학대학', name: '중어중국학과', type: '학과공지' },
+  { category: '인문사회과학대학', name: '광고홍보학과', type: '학과공지' },
+  { category: '인문사회과학대학', name: '일본학과', type: '학과공지' },
+  {
+    category: '인문사회과학대학',
+    name: '미디어커뮤니케이션학과',
+    type: '학과공지',
+  },
+  { category: '인문사회과학대학', name: '영어영문학과', type: '학과공지' },
+  { category: '인문사회과학대학', name: '법학과', type: '학과공지' },
+  { category: '인문사회과학대학', name: '문헌정보학과', type: '학과공지' },
+  { category: '인문사회과학대학', name: '경찰행정학과', type: '학과공지' },
+  {
+    category: '인문사회과학대학',
+    name: '평생교육·청소년상담학과',
+    type: '학과공지',
+  },
+  { category: '인문사회과학대학', name: '소방방재행정학과', type: '학과공지' },
+  { category: '인문사회과학대학', name: '심리학과', type: '학과공지' },
+  { category: '인문사회과학대학', name: '행정학과', type: '학과공지' },
+  { category: '인문사회과학대학', name: '아동학과', type: '학과공지' },
+  { category: '인문사회과학대학', name: '사회복지학과', type: '학과공지' },
+  { category: '인문사회과학대학', name: '디지털콘텐츠학과', type: '학과공지' },
+
+  // 상경대학
+  { category: '상경대학', name: '금융경영학과', type: '학과공지' },
+  { category: '상경대학', name: '경영정보학과', type: '학과공지' },
+  { category: '상경대학', name: '재무부동산학과', type: '학과공지' },
+  { category: '상경대학', name: 'e비지니스학과', type: '학과공지' },
+  { category: '상경대학', name: '스마트항만물류학과', type: '학과공지' },
+  { category: '상경대학', name: '국제관광경영학과', type: '학과공지' },
+  { category: '상경대학', name: '호텔·컨벤션경영학과', type: '학과공지' },
+  { category: '상경대학', name: '무역학과', type: '학과공지' },
+  { category: '상경대학', name: '외식경영학과', type: '학과공지' },
+  { category: '상경대학', name: '유통물류학과', type: '학과공지' },
+  { category: '상경대학', name: '스마트호스피탈리티학과', type: '학과공지' },
+  { category: '상경대학', name: '경영학과', type: '학과공지' },
+  { category: '상경대학', name: '회계학과', type: '학과공지' },
+
+  // 미래융합대학
+  { category: '미래융합대학', name: '부동산자산경영학과', type: '학과공지' },
+  { category: '미래융합대학', name: '뷰티비니지스학과', type: '학과공지' },
+  { category: '미래융합대학', name: '스마트창업경영학과', type: '학과공지' },
+  { category: '미래융합대학', name: 'AI다문화상담학과', type: '학과공지' },
+  { category: '미래융합대학', name: '시니어스포츠지도학과', type: '학과공지' },
+
+  // 의료·보건·생활대학
+  { category: '의료·보건·생활대학', name: '간호학과', type: '학과공지' },
+  { category: '의료·보건·생활대학', name: '임상병리학과', type: '학과공지' },
+  { category: '의료·보건·생활대학', name: '치위생과', type: '학과공지' },
+  { category: '의료·보건·생활대학', name: '방사선학과', type: '학과공지' },
+
+  // 한의과대학
+  { category: '한의과대학', name: '한의예과', type: '학과공지' },
+  { category: '한의과대학', name: '한의학과', type: '학과공지' },
+
+  // 공과대학
+  { category: '공과대학', name: '기계공학과', type: '학과공지' },
+  { category: '공과대학', name: '로봇공학과', type: '학과공지' },
+  { category: '공과대학', name: '자동차공학과', type: '학과공지' },
+  { category: '공과대학', name: '조선해양공학과', type: '학과공지' },
+  { category: '공과대학', name: '신소재공학과', type: '학과공지' },
+  { category: '공과대학', name: '고분자나노공학과', type: '학과공지' },
+  { category: '공과대학', name: '건축학과', type: '학과공지' },
+  { category: '공과대학', name: '건축공학과', type: '학과공지' },
+  { category: '공과대학', name: '토목공학과', type: '학과공지' },
+  { category: '공과대학', name: '도시공학과', type: '학과공지' },
+  { category: '공과대학', name: '환경공학과', type: '학과공지' },
+  { category: '공과대학', name: '화학공학과', type: '학과공지' },
+  { category: '공과대학', name: '화장품공학과', type: '학과공지' },
+  { category: '공과대학', name: '의생명공학과', type: '학과공지' },
+  { category: '공과대학', name: '바이오의약학과', type: '학과공지' },
+  { category: '공과대학', name: '식품공학과', type: '학과공지' },
+  { category: '공과대학', name: '인간공학과', type: '학과공지' },
+  { category: '공과대학', name: '산업ICT기술공학', type: '학과공지' },
+  { category: '공과대학', name: '산업경영빅데이터공학과', type: '학과공지' },
+  { category: '공과대학', name: '전기공학과', type: '학과공지' },
+  { category: '공과대학', name: '전자공학과', type: '학과공지' },
+  { category: '공과대학', name: '정보통신공학과', type: '학과공지' },
+  { category: '공과대학', name: '미래모빌리티학과', type: '학과공지' },
+  { category: '공과대학', name: '첨단에너지공학과', type: '학과공지' },
+
+  // 소프트웨어융합대학
+  {
+    category: '소프트웨어융합대학',
+    name: '소프트웨어융합대학',
+    type: '학과공지',
+  },
+  { category: '소프트웨어융합대학', name: '컴퓨터공학과', type: '학과공지' },
+  {
+    category: '소프트웨어융합대학',
+    name: '컴퓨터소프트웨어공학과',
+    type: '학과공지',
+  },
+  {
+    category: '소프트웨어융합대학',
+    name: '응용소프트웨어공학과',
+    type: '학과공지',
+  },
+  { category: '소프트웨어융합대학', name: '인공지능학과', type: '학과공지' },
+  { category: '소프트웨어융합대학', name: '게임공학과', type: '학과공지' },
+  {
+    category: '소프트웨어융합대학',
+    name: '소프트웨어융합학과',
+    type: '학과공지',
+  },
+]
