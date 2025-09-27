eaf699885485d8d90d51aa251e03e77cad688ee6
임승진 <seungjin051@gmail.com>
2025-09-27 23:50:13 +0900

    feat(lint): add tailwind setting

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -2,4 +2,5 @@ module.exports = {
   trailingComma: 'es5', // 여러 줄을 사용할 때, 후행 콤마 사용 방식
   singleQuote: true, // single 쿼테이션 사용 여부
   semi: false, // 세미콜론 사용 여부
+  plugins: ['prettier-plugin-tailwindcss'],
 }
