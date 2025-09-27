d95a7f76fb6d4dd2c8e85014b7a2afa36aac14f7
임승진 <seungjin051@gmail.com>
2025-09-27 20:00:49 +0900

    feat: config to env setting
    - reset firebase project eky

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -8,13 +8,13 @@ import { getFirestore } from 'firebase/firestore'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyDsQaTirMD8xXUG8_ANVsARVHipNSs__Hk',
-  authDomain: 'hant-teut.firebaseapp.com',
-  projectId: 'hant-teut',
-  storageBucket: 'hant-teut.firebasestorage.app',
-  messagingSenderId: '139799513560',
-  appId: '1:139799513560:web:5550a79be89c03b3d80aad',
-  measurementId: 'G-VJ9LPJ68C0',
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
 }
 
 // Initialize Firebase
