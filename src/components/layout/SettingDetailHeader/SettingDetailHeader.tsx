c61e5e53f16c5213dbdb3f59f2da29a83b1711af
임승진 <seungjin051@gmail.com>
2025-10-23 23:18:46 +0900

    feat: Add SettingDetailHeader component

---
+++ src/components/layout/SettingDetailHeader/SettingDetailHeader.tsx
@@ -0,0 +1,22 @@
+import { Ionicons } from '@expo/vector-icons'
+import { useRouter } from 'expo-router'
+import { TouchableOpacity, Text, View } from 'react-native'
+
+interface SettingDetailHeaderProps {
+  title: string
+}
+
+export default function SettingDetailHeader({
+  title,
+}: SettingDetailHeaderProps) {
+  const router = useRouter()
+
+  return (
+    <View className="flex-row items-center gap-4 bg-white px-4 pt-20">
+      <TouchableOpacity onPress={() => router.back()}>
+        <Ionicons name="arrow-back" size={24} color="black" />
+      </TouchableOpacity>
+      <Text className="text-2xl font-bold">{title}</Text>
+    </View>
+  )
+}
