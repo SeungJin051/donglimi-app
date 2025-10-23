edc68a12f41319e23700208d3b6ab971aec66b27
임승진 <seungjin051@gmail.com>
2025-10-23 23:19:21 +0900

    refactor: settings UI and navigation structure

--- src/components/layout/SettingHeader/SettingHeader.tsx
+++ src/components/layout/SettingHeader/SettingHeader.tsx
@@ -1,24 +1,9 @@
-import { Feather } from '@expo/vector-icons'
-import { DrawerActions } from '@react-navigation/native'
-import { useNavigation, useRouter } from 'expo-router'
 import { Text } from 'react-native'
-import { Platform, TouchableOpacity, View } from 'react-native'
+import { Platform, View } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 export function SettingHeader() {
-  const router = useRouter()
-  const navigation = useNavigation()
   const { top } = useSafeAreaInsets()
-
-  // 사이드바 메뉴 열기
-  const openMenu = () => {
-    navigation.dispatch(DrawerActions.openDrawer())
-  }
-
-  // 마이페이지 설정 페이지로 이동
-  const goToSearch = () => {
-    // router.push('/mypage-settings')
-  }
 
   return (
     <View
@@ -27,13 +12,7 @@ export function SettingHeader() {
         paddingTop: Platform.OS === 'android' ? top + 10 : top,
       }}
     >
-      <TouchableOpacity onPress={openMenu} style={{ padding: 8 }}>
-        <Text className="text-2xl font-semibold">마이페이지</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity onPress={goToSearch} style={{ padding: 8 }}>
-        <Feather name="settings" size={24} color="#999999" />
-      </TouchableOpacity>
+      <Text className="text-2xl font-semibold">설정</Text>
     </View>
   )
 }
