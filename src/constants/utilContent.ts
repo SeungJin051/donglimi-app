9b0d4b4d5f4c4a7d077a449a360d35def4cf4a15
임승진 <seungjin051@gmail.com>
2025-10-23 18:21:40 +0900

    feat: Add university quick links and academic calendar data

---
+++ src/constants/utilContent.ts
@@ -0,0 +1,143 @@
+export const quickItem = [
+  {
+    id: 1,
+    title: '동의대학교 홈',
+    icon: 'home',
+    link: 'https://dap.deu.ac.kr/',
+  },
+  {
+    id: 2,
+    title: 'DAP',
+    icon: 'school',
+    link: 'https://dap.deu.ac.kr/',
+  },
+  {
+    id: 3,
+    title: 'DOOR',
+    icon: 'door-front',
+    link: 'https://door.deu.ac.kr/',
+  },
+  {
+    id: 4,
+    title: '중앙 도서관',
+    icon: 'local-library',
+    link: 'https://lib.deu.ac.kr/',
+  },
+  {
+    id: 5,
+    title: '생활관',
+    icon: 'apartment',
+    link: 'https://dorm.deu.ac.kr',
+  },
+  {
+    id: 6,
+    title: '통학버스',
+    icon: 'directions-bus',
+    link: 'https://www.deu.ac.kr/www/deu-sbus.do',
+  },
+]
+
+export const uniPlanUrl = 'https://www.deu.ac.kr/www/scheduleList.do'
+
+export const uniPlan = [
+  {
+    id: 1,
+    title: '개교기념일',
+    date: '10/22',
+  },
+  {
+    id: 2,
+    title: '중간시험',
+    date: '10/27 ~ 10/31',
+  },
+  {
+    id: 3,
+    title: '수업일수 1/2선',
+    date: '10/29',
+  },
+  {
+    id: 4,
+    title: '학기경과일수 60일째',
+    date: '10/30',
+  },
+  {
+    id: 5,
+    title: '수업일수 2/3선',
+    date: '11/17',
+  },
+  {
+    id: 6,
+    title: '수업일수 3/4선',
+    date: '11/25',
+  },
+  {
+    id: 7,
+    title: '학기경과일수 90일째',
+    date: '11/29',
+  },
+  {
+    id: 8,
+    title: '지정보강일 (10.6/10.7 추석)',
+    date: '12/8 ~ 12/9',
+  },
+  {
+    id: 9,
+    title: '지정보강일 (10.8 대체공휴일)',
+    date: '12/10',
+  },
+  {
+    id: 10,
+    title: '지정보강일 (10.9 한글날)',
+    date: '12/11',
+  },
+  {
+    id: 11,
+    title: '지정보강일 (10.3 개천절)',
+    date: '12/12',
+  },
+  {
+    id: 12,
+    title: '지정보강일 (10.10 임시공휴일)',
+    date: '12/15',
+  },
+  {
+    id: 13,
+    title: '기말시험',
+    date: '12/16 ~ 12/22',
+  },
+  {
+    id: 14,
+    title: '동계방학 시작일',
+    date: '12/23',
+  },
+  {
+    id: 15,
+    title: '성탄절',
+    date: '12/25',
+  },
+  {
+    id: 16,
+    title: '동계계절수업',
+    date: '12/29 ~ 1/19',
+  },
+  {
+    id: 17,
+    title: '신정',
+    date: '1/1',
+  },
+  {
+    id: 18,
+    title: '설날',
+    date: '2/16 ~ 2/18',
+  },
+  {
+    id: 19,
+    title: '현금등록, 수강신청',
+    date: '2/19 ~ 2/24',
+  },
+  {
+    id: 20,
+    title: '2025학년도 전기 학위 수여식',
+    date: '2/20',
+  },
+]
